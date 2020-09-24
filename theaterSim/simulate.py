aa1327e4759abbef0d059ac276a648ed84a1bd79
Ameya <ameyand.98@gmail.com>
2020-09-24 00:57:45 -0500

    started

---
+++ theaterSim/simulate.py
@@ -0,0 +1,15 @@
+# Simulator script for the simulation
+# Author - Ameya Deshmukh
+
+import simpy
+import statistics
+import random
+
+customer_wait_times = []
+
+
+
+
+
+
+
---
+++ theaterSim/theater.py
@@ -0,0 +1,8 @@
+
+
+class Theater(object):
+
+    def __init__(self, env):
+        super().__init__()
+        self.env = env
+
